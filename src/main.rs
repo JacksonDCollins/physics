0a0eba618834f83125766775dd1dea2ab9c4bfbd
Jackson Collins <jdcollins1004@gmail.com>
2023-10-10 10:07:58 +1000

    p3

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-//https://kylemayes.github.io/vulkanalia/drawing/command_buffers.html#command-buffer-allocation
+//https://kylemayes.github.io/vulkanalia/drawing/rendering_and_presentation.html
 #![allow(
     dead_code,
     unused_variables,
@@ -103,6 +103,7 @@ impl App {
         create_pipeline(&device, &mut data)?;
         create_framebuffers(&device, &mut data)?;
         create_command_pool(&instance, &device, &mut data)?;
+        create_command_buffers(&device, &mut data)?;
         Ok(Self {
             entry,
             instance,
@@ -161,6 +162,57 @@ struct AppData {
     pipeline: vk::Pipeline,
     framebuffers: Vec<vk::Framebuffer>,
     command_pool: vk::CommandPool,
+    command_buffers: Vec<vk::CommandBuffer>
+}
+
+unsafe fn create_command_buffers(device: &Device, data: &mut AppData) -> Result<()> {
+    let allocate_info = vk::CommandBufferAllocateInfo::builder()
+    .command_pool(data.command_pool)
+    .level(vk::CommandBufferLevel::PRIMARY)
+    .command_buffer_count(data.framebuffers.len() as u32);
+
+    data.command_buffers = device.allocate_command_buffers(&allocate_info)?;
+
+    //here
+    for (i, command_buffer) in data.command_buffers.iter().enumerate() {
+        let inheritance = vk::CommandBufferInheritanceInfo::builder();
+    
+        let info = vk::CommandBufferBeginInfo::builder()
+            .flags(vk::CommandBufferUsageFlags::empty()) // Optional.
+            .inheritance_info(&inheritance);             // Optional.
+    
+        device.begin_command_buffer(*command_buffer, &info)?;
+
+        let render_area = vk::Rect2D::builder()
+        .offset(vk::Offset2D::default())
+        .extent(data.swapchain_extent);
+
+        let color_clear_value = vk::ClearValue {
+            color: vk::ClearColorValue {
+                float32: [0.0, 0.0, 0.0, 1.0],
+            },
+        };
+
+        let clear_values = &[color_clear_value];
+        let info = vk::RenderPassBeginInfo::builder()
+            .render_pass(data.render_pass)
+            .framebuffer(data.framebuffers[i])
+            .render_area(render_area)
+            .clear_values(clear_values);
+
+        device.cmd_begin_render_pass(
+            *command_buffer, &info, vk::SubpassContents::INLINE);
+
+        device.cmd_bind_pipeline(
+            *command_buffer, vk::PipelineBindPoint::GRAPHICS, data.pipeline);
+            
+        device.cmd_draw(*command_buffer, 3, 1, 0, 0);
+
+        device.cmd_end_render_pass(*command_buffer);
+
+        device.end_command_buffer(*command_buffer)?;
+    }
+    Ok(())
 }
 
 unsafe fn create_command_pool(
